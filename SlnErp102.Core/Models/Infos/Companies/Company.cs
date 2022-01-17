2145e215501675239ac68cda61afe3da7b0bbdaf
murattcetinn <muratcetin7@outlook.com>
2022-01-17 20:20:24 +0300

    .

--- SlnErp102.Core/Models/Infos/Companies/Company.cs
+++ SlnErp102.Core/Models/Infos/Companies/Company.cs
@@ -16,7 +16,7 @@ namespace SlnErp102.Core.Models.Infos.Companies
         public virtual CompanyType? CompanyType { get; set; }
         public virtual IEnumerable<CompanyBranch>? CompanyBranches { get; set; } = new List<CompanyBranch>();
         public virtual IEnumerable<CompanyDetail>? CompanyDetails { get; set; } = new List<CompanyDetail>();
-        public virtual IEnumerable<Product>? Products { get; set; } = new List<Product>();
-        public virtual IEnumerable<ProductEntry>? ProductEntries { get; set; } = new List<ProductEntry>();
+        public virtual IEnumerable<Product>? Products { get; set; } = new List<Product>(); 
+        public virtual IEnumerable<ProductEntry>? ProductEntries { get; set; } = new List<ProductEntry>();  
     }
 }
